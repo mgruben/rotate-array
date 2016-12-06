191bba09daa139a60fbbd8851039eb1e4fd9922d
mgruben <GrubenM@gmail.com>
2016-12-05 23:40:58 -0600

    Add API for solution method

--- Solution.java
+++ Solution.java
@@ -22,6 +22,18 @@
 public class Solution {
 
     /**
+     * Rotate an array of n elements to the right by k steps.
+     * 
+     * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7]
+     * is rotated to [5,6,7,1,2,3,4].
+     * 
+     * @param nums
+     * @param k 
+     */
+    public void rotate(int[] nums, int k) {
+        
+    }
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
